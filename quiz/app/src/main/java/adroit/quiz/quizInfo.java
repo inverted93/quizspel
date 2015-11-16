5d3a1723d52a7f08ab97a32f8f887857b86b5e7b
inverted93 <mr.hoolm@gmail.com>
2015-11-16 21:29:15 +0100

    1.1 Axel

---
+++ quiz/app/src/main/java/adroit/quiz/quizInfo.java
@@ -0,0 +1,26 @@
+package adroit.quiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class quizInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_info);
+    }
+
+
+
+    public void setRating(){
+
+        float f = 1.4f;
+
+
+
+    }
+
+
+
+}
