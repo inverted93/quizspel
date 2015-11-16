9e0127d88eab7cfc4d039ca93996cf8335cb4163
VanDykeBrown <alexanderbackman.93@gmail.com>
2015-11-16 17:47:43 +0100

    Skapade en listview

---
+++ quiz/app/src/main/java/adroit/quiz/quizMain.java
@@ -0,0 +1,25 @@
+package adroit.quiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+public class quizMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_main);
+
+
+        String[] games = {"Star Wars", "Potato-quiz", "Stromstad", "Horses", "Dogs", "WW2", "Trump"};
+        ListAdapter gameAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, games);
+        ListView gameList = (ListView) findViewById(R.id.gameList);
+        gameList.setAdapter(gameAdapter);
+
+    }
+}
