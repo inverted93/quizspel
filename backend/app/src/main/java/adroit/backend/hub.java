556c2cf26a5fa67652cb5e46d7bebff104077fe2
inverted93 <mr.hoolm@gmail.com>
2015-11-18 17:28:37 +0100

    Axel Backend nätverk

--- backend/app/src/main/java/adroit/backend/hub.java
+++ backend/app/src/main/java/adroit/backend/hub.java
@@ -5,7 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class hub extends AppCompatActivity {
+import org.json.JSONObject;
+import org.json.JSONException;
+import java.io.IOException;
+
+
+public class hub extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +30,22 @@ public class hub extends AppCompatActivity {
 
     }
 
-    public void changePageMyQuiz(View v){
+    public void changePageMyQuiz(View v) throws IOException, JSONException{
 
-        Intent myIntent = new Intent(this, myQuiz.class);
+        //Intent myIntent = new Intent(this, myQuiz.class);     DETTA SKA TILLBAKA SEN
+        //startActivity(myIntent);
 
-        startActivity(myIntent);
+        System.out.println("Vi kom till hubben");
+
+        JSONObject json = jsonConnection.readJsonFromUrl("https://www.dropbox.com/s/834wejq3i3w302v/test.json?dl=0");
+
+        System.out.println("Rad:   " + json.toString());
+        System.out.println("ID:    " + json.get("id"));
+
+        jsonConnection.testMetod();
+
+
+
 
 
 
