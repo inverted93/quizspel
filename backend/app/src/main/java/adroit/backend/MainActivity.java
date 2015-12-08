49c358aeaa16d9c467544c6bbcfda4eeb3f072d3
inverted93 <mr.hoolm@gmail.com>
2015-12-08 18:09:22 +0100

    CreateQuiz fix

--- backend/app/src/main/java/adroit/backend/MainActivity.java
+++ backend/app/src/main/java/adroit/backend/MainActivity.java
@@ -47,7 +47,6 @@ public class MainActivity extends AppCompatActivity{
         new retrieveData().execute();  // VIktig rad
 
 
-        createQuestion.fillList();
 
 
     }
--- backend/app/src/main/java/adroit/backend/createQuestion.java
+++ backend/app/src/main/java/adroit/backend/createQuestion.java
@@ -21,49 +21,73 @@ public class createQuestion extends AppCompatActivity {
 
 
 
-    static JSONObject questTmp = new JSONObject();
-    static JSONObject ansTmp = new JSONObject();
-
-    static JSONArray questArr = new JSONArray();
-    static JSONArray ansArr = new JSONArray();
 
 
-    public static void fillList(){
+
+
+
+    JSONArray questArr = new JSONArray();
+    JSONArray ansArr = new JSONArray();
+
+    int pc=1; // PageCount
+
+
+    public void getText(View v){
+
+        EditText question = (EditText) findViewById(R.id.editText5);
+
+        EditText ans1 = (EditText) findViewById(R.id.editText6);
+        EditText ans2 = (EditText) findViewById(R.id.editText7);
+        EditText ans3 = (EditText) findViewById(R.id.editText8);
+        EditText ans4 = (EditText) findViewById(R.id.editText9);
 
         try{
 
 
+        String questionString = question.getText().toString();
+        String ans1String = ans1.getText().toString();
+        String ans2String = ans2.getText().toString();
+        String ans3String = ans3.getText().toString();
+        String ans4String = ans4.getText().toString();
+
+        Log.d("1:" +questionString, ":2:" + ans1String + ans2String + ans3String + ans4String);
 
 
 
-            questTmp.put("id", "1 ");
-            questTmp.put("name", "Axel");
-
-            questArr.put(0, questTmp);
-
-            questTmp.put("id", "2 ");
-            questTmp.put("name", "Mattis");
-
-            questArr.put(1, questTmp);
 
 
-            questTmp.put("id", "3 ");
-            questTmp.put("name", "Holm");
+            JSONObject questTmp = new JSONObject();
+            JSONObject ansTmp = new JSONObject();
 
-            questArr.put(2, questTmp);
+            questTmp.put("question", "" + questionString);
+            questArr.put(questTmp);
 
-            //for(int i=0;i<6;i++){}
+            ansTmp.put("ans1", "" + ans1String);
+            ansTmp.put("ans2", "" + ans2String);
+            ansTmp.put("ans3", "" + ans3String);
+            ansTmp.put("ans4", "" + ans4String);
+            ansArr.put(pc, ansTmp);
 
 
             JSONObject tmp;
-            for(int j=0;j<questArr.length();j++){
+//
+            tmp = ansArr.getJSONObject(1);
 
-                tmp = questArr.getJSONObject(j);
-                String s = tmp.getString("id");
-                String s2 = tmp.getString("name");
-                Log.d("Vi kom helt fram:" + s2, ":909:" + s + j);
 
+            Log.d("HEJSANSVEJSAN", "1:" + tmp.getString("ans1") + tmp.getString("ans2") + tmp.getString("ans3") + tmp.getString("ans4") + tmp.getString("ans5"));
+
+
+
+
+
+
+            if(questionString.isEmpty()||ans1String.isEmpty()||ans2String.isEmpty()||ans3String.isEmpty()){  //Borjan pa en checkIfEmptyMetod
+                Log.d("True", "1");
+            }else{
+                Log.d("false", "1");
             }
+
+
 
 
 
@@ -75,6 +99,43 @@ public class createQuestion extends AppCompatActivity {
     }
 
 
+
+    public void fillFields(){
+
+
+
+    }
+
+
+    public void Create(){
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     public void changePage(View v){ //Ska bort sen
 
 
@@ -84,53 +145,9 @@ public class createQuestion extends AppCompatActivity {
         startActivity(myIntent);
     }
 
-    EditText question;
-
-    EditText ans1;
-    EditText ans2;
-    EditText ans3;
-    EditText ans4;
 
 
 
-    public void readLines(View v){
-
-        question = (EditText) findViewById(R.id.editText5);
-
-        ans1 = (EditText) findViewById(R.id.editText6);
-        ans2 = (EditText) findViewById(R.id.editText7);
-        ans3 = (EditText) findViewById(R.id.editText8);
-        ans4 = (EditText) findViewById(R.id.editText9);
-
-
-        String questionString = question.getText().toString();
-        String ans1String = question.getText().toString();
-        String ans2String = question.getText().toString();
-        String ans3String = question.getText().toString();
-        String ans4String = question.getText().toString();
-
-
-
-        if(questionString.isEmpty()||ans1String.isEmpty()||ans2String.isEmpty()||ans3String.isEmpty()){
-
-
-            Log.d("True", "1");
-
-
-        }else{
-
-            Log.d("false", "1");
-
-        }
-
-
-
-
-
-
-
-
-    }
 
 
 
