5d5d02a2f656cc8d819b88c14987ed3c2bb984aa
Jonas9307 <jonas9307@gmail.com>
2016-01-10 20:22:27 +0100

    Log out func button & dialog

--- backend/app/src/main/java/adroit/backend/hub.java
+++ backend/app/src/main/java/adroit/backend/hub.java
@@ -1,5 +1,7 @@
 package adroit.backend;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -101,12 +103,6 @@ public class hub extends AppCompatActivity{
     }
 
 
-
-
-
-
-
-
     public void changePage(View v){
 
         Intent myIntent = new Intent(this, createQuiz.class);
@@ -123,7 +119,41 @@ public class hub extends AppCompatActivity{
     }
 
 
+    public void changePageToMain(View view){
 
+        Intent myIntent = new Intent(this, MainActivity.class);
+        startActivity(myIntent);
+        finish();
+        overridePendingTransition(0, 0);
+
+    }
+
+    public void onBackPressed()
+    {
+
+        AlertDialog.Builder adbuilder = new AlertDialog.Builder(hub.this);
+        adbuilder.setMessage("Are you sure you want to log out?");
+        adbuilder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int which) {
+
+                Intent i = new Intent(hub.this, MainActivity.class);
+                startActivity(i);
+                finish();
+                overridePendingTransition(0, 0);
+
+            }
+        });
+        adbuilder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        AlertDialog alert = adbuilder.create();
+        alert.show();
+
+    }
 
 
 
--- quiz/app/src/main/java/adroit/quiz/hub.java
+++ quiz/app/src/main/java/adroit/quiz/hub.java
@@ -1,5 +1,7 @@
 package adroit.quiz;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -40,13 +42,39 @@ public class hub extends AppCompatActivity {
         }
     }
 
-    public void onBackPressed()
-    {
-        super.onBackPressed();
-        Intent i = new Intent(this, MainActivity.class);
-        startActivity(i);
+    public void changePageToMain(View view){
+
+        Intent myIntent = new Intent(this, MainActivity.class);
+        startActivity(myIntent);
         finish();
         overridePendingTransition(0, 0);
+
+    }
+
+    public void onBackPressed()
+    {
+
+        AlertDialog.Builder adbuilder = new AlertDialog.Builder(hub.this);
+        adbuilder.setMessage("Are you sure you want to log out?");
+        adbuilder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int which) {
+
+                Intent i = new Intent(hub.this, MainActivity.class);
+                startActivity(i);
+                finish();
+                overridePendingTransition(0, 0);
+
+            }
+        });
+        adbuilder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        AlertDialog alert = adbuilder.create();
+        alert.show();
 
     }
 
