371b6067f0be8e57d76919bdaa60658b472403e2
inverted93 <mr.hoolm@gmail.com>
2016-01-14 21:05:31 +0100

    kommentarer

--- backend/app/src/main/java/adroit/backend/hub.java
+++ backend/app/src/main/java/adroit/backend/hub.java
@@ -24,15 +24,17 @@ public class hub extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hub);
+        //Ett infomeddelande sätts till osynligt
         ((TextView) findViewById(R.id.textView7)).setVisibility(View.INVISIBLE);
+        //En metod som fyller statsen i hub anropas
         fillStats();
     }
 
-
+    //id till den som är inloggad hämtas ifrån MainActivity
     String id = MainActivity.getId();
 
     static JSONObject jobj = new JSONObject();
-
+    // Json objektet skickas ifrån retreiveData.
     public static void setJson(JSONObject j){
         jobj= j;
     }
@@ -45,6 +47,7 @@ public class hub extends AppCompatActivity{
             startActivity(startBackend);
         }
         catch (Exception e) {
+            //Felmeddelande visas
             ((TextView) findViewById(R.id.textView7)).setText(getString(R.string.errorMessageHub));
             ((TextView) findViewById(R.id.textView7)).setVisibility(View.VISIBLE);
 
@@ -53,35 +56,37 @@ public class hub extends AppCompatActivity{
 
 
     public void fillStats(){
-
-
-
+        /*Statsen som visas fylls på*/
+        //TextViewn som ska visa statsen identifieras
         TextView corrView = (TextView)findViewById(R.id.textView3);
 
         try{
-
+            //Json arrayen members hämtas
             JSONArray membArr = jobj.getJSONArray("Members");
+            //Temporära behållare skapas
             String qA ="";
             String cA ="";
 
+            //En loop snurrar igenom memberArr
             for(int i=0; i<membArr.length(); i++){
-
+                //Json-objektet som ligger på pos i hämtas
                 JSONObject tmpObj = membArr.getJSONObject(i);
+                //Hämtar id och kollar om det är rätt id, om så är fallet så hämtas data ifrån det kontot.
                 String userId = tmpObj.getString("UserID");
                 if(userId.equals(id)){
-
                     qA = tmpObj.getString("QuestionsAnswered");
                     cA = tmpObj.getString("RightAnswers");
                 }
             }
-
+            //Statsen parsas om till doubles
             double qADouble = Double.parseDouble(qA);
             double cADouble = Double.parseDouble(cA);
 
+            //Skapar en formaterare som inte visar några decimaler
             NumberFormat formatter = new DecimalFormat("#0");
-
+            //Räknar ut hur många procent rätt användaren har svarat
             String percent = formatter.format((cADouble / qADouble) * 100);
-
+            //Sätter texten till det som ska visas
             corrView.setText("Questions answered\n" + qA + "\n\nRight answers percentage\n" + percent + "%");
 
         }catch(JSONException e){
@@ -90,14 +95,14 @@ public class hub extends AppCompatActivity{
     }
 
     public void changePage(View v){
-
+        //Byter aktivitet till createQuiz
         Intent myIntent = new Intent(this, createQuiz.class);
         startActivity(myIntent);
 
     }
 
     public void changePageMyQuiz(View v) {
-
+        //Byter aktivitet till myQuiz
         Intent myIntent = new Intent(this, myQuiz.class);
         startActivity(myIntent);
 
@@ -105,7 +110,7 @@ public class hub extends AppCompatActivity{
 
 
     public void changePageToMain(View view){
-
+        //Byter aktivitet till MainActivity
         Intent myIntent = new Intent(this, MainActivity.class);
         startActivity(myIntent);
         finish();
@@ -115,7 +120,8 @@ public class hub extends AppCompatActivity{
 
     public void onBackPressed()
     {
-
+        //Om användaren trycker på tillbaka knappen så ställs frågan "Vill du logga ut?"
+        //Om användaren trycker ja så skickas användaren tillbaka till MailActivity
         AlertDialog.Builder adbuilder = new AlertDialog.Builder(hub.this);
         adbuilder.setMessage("Are you sure you want to log out?");
         adbuilder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
@@ -129,6 +135,7 @@ public class hub extends AppCompatActivity{
 
             }
         });
+        //Om användaren säger nej så avslutas dialogrutan.
         adbuilder.setNegativeButton("No", new DialogInterface.OnClickListener() {
 
             public void onClick(DialogInterface dialog, int which) {
